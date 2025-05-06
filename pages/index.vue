4d6fc7c25476dd0695021f13a38245ed1c2d5e89
Tomas Parra <tomasparra@Tomass-iMac.local>
2025-05-06 16:31:50 +0200

    basic layout ready

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,27 @@
 <script setup>
+import { UCard, UContainer } from '#components';
 import { useBoardStore } from '~/stores/boardStore';
 
 const boardStore = useBoardStore()
 </script>
 
 <template>
-   <main>
-    <h1>{{ boardStore.board.name }}</h1>
-    <div v-for="column in boardStore.board.columns" :key="column.name">
-        <h2>{{ column.name }}</h2>
-        <ul>
-            <li v-for="task in column.tasks" :key="task.id">
-                <strong>{{ task.name }}</strong>
-                <p>{{ task.description }}</p>
-            </li>
-        </ul>
-    </div>
-   </main>
+   <div class="board-wrapper">
+    <main class="board">
+        <UContainer class="column" v-for="column in boardStore.board.columns" :key="column.name">
+            <h2 class="mb-4">{{ column.name }}</h2>
+            <ul>
+                <li v-for="task in column.tasks" :key="task.id">
+                   <UCard class="mb-4">
+                    <strong>{{ task.name }}</strong>
+                    <p>{{ task.description }}</p>
+                   </UCard>
+                </li>
+            </ul>
+        </UContainer>
+        <UContainer>
+            <input type="text" placeholder="+ Create new column">
+        </UContainer>
+       </main>
+   </div>
 </template>
